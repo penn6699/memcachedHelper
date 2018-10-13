499eca706e51967df38aa3c161d0df9a999a3dc9
penn6699 <penn6699@126.com>
2018-10-13 09:33:57 +0800

    1

---
+++ App_Code/MemcachedHelper.cs
@@ -0,0 +1,37 @@
+﻿using Enyim.Caching;
+using Enyim.Caching.Memcached;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// MemcachedHelper 的摘要说明
+/// </summary>
+public class MemcachedHelper
+{
+    
+    private MemcachedClient mc;
+
+    public MemcachedHelper() {
+        mc = new MemcachedClient();
+    }
+
+    public bool Set(string key,object value) {
+        return mc.Store(StoreMode.Set, key, value);
+    }
+
+    public T Get<T>(string key) 
+    {
+        return mc.Get<T>(key);
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack) {
+            MemcachedHelper mch = new MemcachedHelper();
+
+            string key= Request["key"] ?? "a";
+            string value = Request["value"] ?? "0";
+            if (mch.Set(key, value))
+                Response.Write("保存成功");
+
+            Response.Write(mch.Get<string>(key));
+        }
+    }
+}
